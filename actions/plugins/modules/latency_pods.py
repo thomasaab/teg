b318fbbde9e9726edd101a67a4a5a83c63eda9ff
thomas <thomalfonso@gmail.com>
2021-01-07 11:42:58 -0400

    test latency 12

--- actions/plugins/modules/latency_pods.py
+++ actions/plugins/modules/latency_pods.py
@@ -107,7 +107,7 @@ def exect_pod(name, namespace, module):
     exec_command = [
         '/bin/sh',
         '-c',
-        'ls -a',
+        'tc qdisc add dev eth0 root netem delay 100ms; ping –c 15 google.com',
         ]
     resp = stream(api_instance.connect_get_namespaced_pod_exec,
                   name,
