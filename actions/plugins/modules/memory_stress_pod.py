e3408adf3fc0cc3bf8c9687612c7201b2ab9412e
thomas <thomalfonso@gmail.com>
2021-03-07 18:35:57 -0400

    add stress disk, memory and cpu modules

---
+++ actions/plugins/modules/memory_stress_pod.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python
+
+"""
+Copyright 2019 Pystol (pystol.org).
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+"""
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: memory_stress_pod
+
+short_description: A module that stress memory pods
+
+version_added: "2.8"
+
+description:
+    - "A module that stress memory pods."
+
+
+options:
+    namespace:
+        default: default
+    distribution:
+        default: poisson
+    amount:
+        default: 10
+
+author:
+    - "Carlos Camacho (@ccamacho)"
+'''
+
+EXAMPLES = '''
+# Pass in a message
+- name: Test with a message
+  memory_stress_pod:
+    namespace: default
+    distribution: poisson
+    amount: 10
+'''
+
+RETURN = '''
+fact:
+    description: Actual facts
+    type: str
+    sample: Jane Doe is a smart cookie.
+'''
+
+FACTS = [
+    "{name} is looking great today!",
+    "{name} is a smart cookie.",
+    "I’d choose {name}'s company over pizza anytime."
+]
+
+
+import matplotlib
+matplotlib.use('Agg')
+import random
+from ansible.module_utils.basic import AnsibleModule
+from scipy.stats import poisson
+import matplotlib.pyplot as plt
+from kubernetes import client
+from kubernetes.client.rest import ApiException
+import time
+from kubernetes.stream import stream
+import datetime
+import time
+import os 
+import subprocess
+
+from ansible_collections.pystol.actions.plugins.module_utils.k8s_common import load_kubernetes_config
+
+
+global_available = []
+global_unavailable = []
+global_kill = []
+
+
+def inyect_memory(name, namespace, module):
+    api_instance = client.CoreV1Api()
+    module.log(msg="pod: " + name)
+    try:
+        resp = api_instance.read_namespaced_pod(name=name, namespace=namespace)
+    except ApiException as e:
+        if e.status != 404:
+            print("Unknown error: %s" % e)
+            exit(1)
+        # Calling exec and waiting for response
+    exec_command = [
+        '/bin/sh',
+        '-c',
+        'apt update; apt-get install -y stress-ng; stress-ng --version; stress-ng --vm 1 --vm-bytes 75% --vm-method all --verify -t 2m -v',
+        ]
+    resp = stream(api_instance.connect_get_namespaced_pod_exec,
+                  name,
+                  'default',
+                  command=exec_command,
+                  stderr=True, stdin=False,
+                  stdout=True, tty=False)
+    print("Response: " + resp)
+
+def get_pods(namespace=''):
+    api_instance = client.CoreV1Api()
+    try:
+        if namespace == '':
+            api_response = api_instance.list_pod_for_all_namespaces()
+        else:
+            api_response = api_instance.list_namespaced_pod(
+                namespace,
+                field_selector='status.phase=Running')
+        return api_response
+    except ApiException as e:
+        print("CoreV1Api->list_pod_for_all_namespaces: %s\n" % e)
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        namespace=dict(type='str', required=True),
+        distribution=dict(type='str', required=True),
+        amount=dict(type='int', required=True),
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    rc = 0
+    stderr = "err"
+    stderr_lines = ["errl1", "errl2"]
+    stdout = "out"
+    stdout_lines = ["outl1", "outl1"]
+
+    module.log(msg='test!!!!!!!!!!!!!!!!!')
+
+    namespace = module.params['namespace']
+    amount = module.params['amount']
+
+    result = dict(
+        changed=True,
+        stdout=stdout,
+        stdout_lines=stdout_lines,
+        stderr=stderr,
+        stderr_lines=stderr_lines,
+        rc=rc,
+    )
+
+    result['fact'] = random.choice(FACTS).format(
+        name=module.params['namespace']
+    )
+
+
+    # random numbers from poisson distribution
+    n = amount
+    a = 0
+    data_poisson = poisson.rvs(mu=10, size=n, loc=a)
+    counts, bins, bars = plt.hist(data_poisson)
+    plt.close()
+    load_kubernetes_config()
+    configuration = client.Configuration()
+    configuration.assert_hostname = False
+    client.api_client.ApiClient(configuration=configuration)
+
+
+    for experiment in counts:
+        pod_list = get_pods(namespace=namespace)
+        aux_li = []
+        for fil in pod_list.items:
+            if fil.status.phase == "Running":
+                aux_li.append(fil)
+        pod_list = aux_li
+
+        # From the Running pods I randomly choose those to die
+        # based on the histogram length
+        print("-------")
+        print("Pod list length: " + str(len(pod_list)))
+        print("Number of pods to get: " + str(int(experiment)))
+        print("-------")
+        # In the case of the experiment being longer than the pod list,
+        # then the maximum will be the lenght of the pod list
+        if (int(experiment) > len(pod_list)):
+            to_be_memory = random.sample(pod_list, len(pod_list))
+        else:
+            to_be_memory = random.sample(pod_list, int(experiment))
+
+        for pod in to_be_memory:
+            inyect_memory(pod.metadata.name, pod.metadata.namespace, module)
+        global_kill.append((datetime.datetime.now(), int(experiment)))
+        # time.sleep(10)
+        print(datetime.datetime.now())
+    print("Ending histogram execution")
+
+    if module.check_mode:
+        return result
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
---
+++ actions/roles/diskstresspod/README.md
@@ -0,0 +1,93 @@
+# KillPods role
+
+This is	a role to execute the action to	kill randomly a
+set of pods.
+
+Run with:
+
+This action needs 3 required parameters.
+
+* pystol_killpods_namespace: The namespace for killig the pods
+* pystol_killpods_distribution: The probabilistic distribution to create the histogram
+* pystol_killpods_amount: The total amount of pods to kill
+
+## Execute the Pystol action in the cluster using the operator
+
+
+```
+pystol run --namespace pystol \
+           --collection actions \
+           --role killpods \
+           --extra-vars '{"pystol_killpods_namespace":"default","pystol_killpods_distribution":"poisson","pystol_killpods_amount":3}'
+
+```
+
+Or locally with, copying the `ansible -m ...` command from the
+container execution info,
+the containers running the actions are named as
+**pystol-action-pystol-actions-actionname-d34de-2345r**.
+Open the container details and execute the
+arguments.
+
+The actions execution should look like:
+
+```
+echo '---' > req.yml; \
+echo 'collections:' >> req.yml; \
+echo '- name: pystol.actions' >> req.yml;\
+echo '  source: https://galaxy.ansible.com' >> req.yml; \
+ansible-galaxy collection install --force -r req.yml; \
+ansible -m include_role -a 'name=pystol.actions.killpods' -e '{'pystol_killpods_namespace': 'default', 'pystol_killpods_distribution': 'poisson', 'pystol_killpods_amount': '1', 'ansible_python_interpreter': '/usr/bin/python3', 'pystol_action_id': 'pystol-action-pystol-actions-killpods-nthkp'}' localhost -vv; exit 0
+```
+
+## Local testing of the KillPods action/role
+
+If you don't require to execute the action from the
+cluster operator, for example for developing purposes,
+you can follow the next steps.
+
+Open a session in a terminal and execute:
+
+```
+journalctl -f
+```
+
+This will print the system's logs including the debug information from the
+action.
+
+In another session execute:
+
+```
+# Go to the home folder
+cd
+# Clone pystol-galaxy
+git clone https://github.com/pystol/pystol-galaxy.git
+
+# Do any change you need in the repository.
+
+# Then execute
+cd ~/pystol-galaxy/actions/
+mkdir -p releases
+# Build and install the collection
+ansible-galaxy collection build -v --force --output-path releases/
+cd releases
+LATEST=$(ls pystol-actions*.tar.gz | grep -v latest | sort -V | tail -n1)
+ln -sf $LATEST pystol-actions-latest.tar.gz
+ansible-galaxy collection install --force pystol-actions-latest.tar.gz
+
+# Go to the playbook folder
+cd ~/pystol-galaxy/actions/roles/killpods/tasks/
+# Execute the playbook
+ansible -m include_role \
+        -a 'name=pystol.actions.killpods' \
+        -e '{'pystol_killpods_namespace': 'default',
+             'pystol_killpods_distribution': 'poisson',
+             'pystol_killpods_amount': '1',
+             'ansible_python_interpreter': '/usr/bin/python3'}' \
+        localhost \
+        -vvvvv
+```
+
+This way, you should be able to test locally
+all the actions without the need of deploying them
+in the Kubernetes cluster.
